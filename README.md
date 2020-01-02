b0064b1f05badd4ca71ec3d17daae9d7998715f8
opti21 <browe.media@gmail.com>
2020-01-02 17:34:36 -0600

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# whos-live
+ A client to keep track of when the streams I mod on Twitch go live
---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const axios = require("axios");
+
+const port = process.env.PORT || 4001;
+const index = require("./routes/index");
+
+const app = express();
+app.use(index);
+
+const server = http.createServer(app);
+
+const io = socketIo(server);
+
+io.on("connection", socket => {
+  console.log("New client client connected");
+})
+
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
